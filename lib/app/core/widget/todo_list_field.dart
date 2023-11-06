a1371c0d1afe658261127b591b1c2fd6c2b07165
Itajacy BSD <itajacy@bsd.com.br>
2023-11-06 13:03:19 -0300

    uso do ValueNotifier para verificar se o botão do olho foi clicado e alterar o botão e exibir ou não o texto conforme obscureText

--- lib/app/core/widget/todo_list_field.dart
+++ lib/app/core/widget/todo_list_field.dart
@@ -6,44 +6,62 @@ class TodoListField extends StatelessWidget {
   final String label;
   final IconButton? suffixIconButton;
   final bool obscureText;
+  final ValueNotifier<bool> obscureTextVN;
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
 
   TodoListField({
     Key? key,
     required this.label,
     this.suffixIconButton,
     this.obscureText = false,
+    this.controller,
+    this.validator,
   })  : assert(obscureText == true ? suffixIconButton == null : true,
             "obscureText não pode ser enviado em conjunto com suffixIconButton "),
+        obscureTextVN = ValueNotifier(obscureText),
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle: TextStyle(
-          fontSize: 15,
-          color: Colors.black,
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30),
-          borderSide: BorderSide(color: Colors.red),
-        ),
-        isDense: true, //torna mais denso, os formulários ficam mais próximos
-        suffixIcon: this.suffixIconButton ??
-            (obscureText == true
-                ? IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      TodoListIcons.eye,
-                      size: 15,
-                    ))
-                : null),
-      ),
-      obscureText: obscureText,
+    return ValueListenableBuilder<bool>(
+      valueListenable: obscureTextVN,
+      builder: (_, obscureTextValue, child) {
+        return TextFormField(
+          controller: controller,
+          validator: validator,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(color: Colors.red),
+            ),
+            isDense:
+                true, //torna mais denso, os formulários ficam mais próximos
+            suffixIcon: this.suffixIconButton ??
+                (obscureText == true
+                    ? IconButton(
+                        onPressed: () {
+                          obscureTextVN.value = !obscureTextValue;
+                        },
+                        icon: Icon(
+                          !obscureTextValue
+                              ? TodoListIcons.eye_slash
+                              : TodoListIcons.eye,
+                          size: 15,
+                        ))
+                    : null),
+          ),
+          obscureText: obscureTextValue,
+        );
+      },
     );
   }
 }
