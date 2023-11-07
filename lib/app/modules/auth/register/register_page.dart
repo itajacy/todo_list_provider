444fc8364138ee72633af52d182856f4b9dcd416
Itajacy BSD <itajacy@bsd.com.br>
2023-11-07 15:43:18 -0300

    alterado register_page.dart AppBar alterado, construído manualmente o botão voltar

--- lib/app/modules/auth/register/register_page.dart
+++ lib/app/modules/auth/register/register_page.dart
@@ -7,7 +7,17 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: ClipOval(
+            child: Container(
+              child: Icon(Icons.arrow_back_ios_new_outlined),
+            ),
+          ),
+        ),
       ),
       body: Container(),
     );
