47dd6803ecc468335393cf463b1e7c5382d9ca7c
Itajacy BSD <itajacy@bsd.com.br>
2023-11-08 08:20:41 -0300

    finalização da AppBar do register_page.dart

--- lib/app/modules/auth/register/register_page.dart
+++ lib/app/modules/auth/register/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -7,15 +8,31 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: false,
         automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Todo List',
+              style: TextStyle(fontSize: 10, color: context.primaryColor),
+            ),
+            Text('Cadastro',
+                style: TextStyle(fontSize: 12, color: context.primaryColor)),
+          ],
+        ),
         leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-            minuto 3:12
-          },
+          onPressed: () => Navigator.pop(context),
           icon: ClipOval(
             child: Container(
-              child: Icon(Icons.arrow_back_ios_new_outlined),
+              color: context.primaryColor.withAlpha(20),
+              padding: const EdgeInsets.all(8),
+              child: Icon(
+                Icons.arrow_back_ios_new_outlined,
+                size: 20,
+                color: context.primaryColor,
+              ),
             ),
           ),
         ),
