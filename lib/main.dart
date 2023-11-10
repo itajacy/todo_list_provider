843b5e942e0224c3da15e2ac601857ba2abac4a6
Itajacy BSD <itajacy@bsd.com.br>
2023-11-10 15:55:22 -0300

    alterado o main.dart conforme a aula e não conforme o pub.dev em https://firebase.flutter.dev/docs/overview

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 //  SHA1:  3B:2E:4A:A3:5B:79:8B:3A:52:75:F5:6C:3A:29:0B:4C:18:0E:55:1A
-// SHA256: AB:F0:11:DB:A9:BA:7C:DB:E9:2A:F5:44:7A:4D:68:5A:67:50:2E:9A:9D:AF:91:15:ED:31:18:0F:C1:C0:01:8D
-// Válido de: Mon Apr 24 16:11:13 GMT-03:00 2023 até: Wed Apr 16 16:11:13 GMT-03:00 2053
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list_provider/app/app_module.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const AppModule());
 }
