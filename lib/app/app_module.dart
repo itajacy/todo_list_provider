369f995fc187c9bdfd14d6fa978e88c65517cb40
itajacy <itajacy@gmail.com>
2023-11-15 20:07:28 -0300

    app_module.dart adicionando os Providers e as dependencias

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/app_widget.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_migration_factory.dart';
+import 'package:todo_list_provider/app/repositories/user/user_repository.dart';
+import 'package:todo_list_provider/app/repositories/user/user_repository_impl.dart';
+import 'package:todo_list_provider/app/services/user/user_service.dart';
+import 'package:todo_list_provider/app/services/user/user_service_impl.dart';
 
 class AppModule extends StatelessWidget {
   const AppModule({super.key});
@@ -10,11 +15,19 @@ class AppModule extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        Provider(create: (_) => FirebaseAuth.instance),
         Provider(
           create: (context) => SqliteMigrationFactory(),
           //! não será preguiçoso, executará e criará a instancia do sqlite na primeira vez
           //! criando banco de dados, executando as migrations por isso deve ficar como "false"
           lazy: false,
+        ),
+        //! o context.read() buscando do Provider a instancia do firebaseAuth
+        Provider<UserRepository>(
+          create: (context) => UserRepositoryImpl(firebaseAuth: context.read()),
+        ),
+        Provider<UserService>(
+          create: (context) => UserServiceImpl(userRepository: context.read()),
         ),
       ],
       child: AppWidget(),
