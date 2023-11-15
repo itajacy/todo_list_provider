7497e11ad76ad3a640da8455a9466a8d2331a8d8
itajacy <itajacy@gmail.com>
2023-11-15 19:30:51 -0300

    modificado o user_repository.dart e criados o arquivos da pasta services/user/  user_service.dart e user_service_impl.dart

--- lib/app/repositories/user/user_repository.dart
+++ lib/app/repositories/user/user_repository.dart
@@ -1,5 +1,7 @@
-import './user_repository.dart';
 
-class UserRepository implements UserRepository {
 
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class UserRepository {
+  Future<User?> register(String email, String password);
 }
---
+++ lib/app/services/user/user_service_impl.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import './user_service.dart';
+
+class UserServiceImpl implements UserService {
+  @override
+  Future<User?> register(String email, String password) {
+    // TODO: implement register
+    throw UnimplementedError();
+  }
+
+}
