83f887ae8098d0d1c7888e3d1fc1fe700364d308
itajacy <itajacy@gmail.com>
2023-11-21 19:14:57 -0300

    criada a classe Messages.dart para centralização das mensagens

--- lib/app/modules/auth/register/register_controller.dart
+++ lib/app/modules/auth/register/register_controller.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:todo_list_provider/app/exception/auth_exception.dart';
