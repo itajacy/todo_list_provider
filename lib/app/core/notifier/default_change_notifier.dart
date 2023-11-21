b5fbcb25b9e250104ae26f12108249b8e5db6bac
itajacy <itajacy@gmail.com>
2023-11-21 19:34:37 -0300

    criado o default_chage_notifier.dart que será a classe que todas as nossas classes irão extender ao invés de ChangeNotifier usaremos a nossa

--- lib/app/core/notifier/default_change_notifier.dart
+++ lib/app/core/notifier/default_change_notifier.dart
@@ -1,4 +1,27 @@
+//! Classe que todas as nossas controllers irão extender
+import 'package:flutter/material.dart';
 
-class DefaultChangeNotifier {
+class DefaultChangeNotifier extends ChangeNotifier{
   
+  bool _loading = false;
+  String? _error;
+  bool _success = false;
+
+//! encapsulando o loading, ninguem pode alterar o loading a não ser que seja dentro 
+//! dessa classe
+  bool get loading => _loading;
+  String? get error => _error;
+  bool get hasError => _error != null;
+  bool get isSuccess => _success;
+  void showLoading() => _loading = true;
+  void hideLoading() => _loading = false;
+
+  void success() => _success = true;
+
+  void setError(String? error) => _error = error;
+
+
+
+
+
 }
