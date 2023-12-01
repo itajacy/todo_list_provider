90e98d50f67b164702b1d9fba3ce3ffcf038d75f
Itajacy BSD <itajacy@bsd.com.br>
2023-12-01 10:48:05 -0300

    Navegacao sem contexto finalizada  todo_list_navigator.dart e app_widget.dart

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
+import 'package:todo_list_provider/app/core/navigator/todo_list_navigator.dart';
 import 'package:todo_list_provider/app/core/ui/todo_list_ui_config.dart';
 import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
 import 'package:todo_list_provider/app/modules/home/home_module.dart';
@@ -37,6 +38,7 @@ class _AppWidgetState extends State<AppWidget> {
       title: "TODO LIST Provider",
       //initialRoute: '/login',
       theme: TodoListUiConfig.theme,
+      navigatorKey: TodoListNavigator.navigatorKey,
       routes: {
         ...AuthModule().routers,
         ...HomeModule().routers,
