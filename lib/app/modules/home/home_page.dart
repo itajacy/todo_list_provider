1262228a388c7956b045afa0999eb9ac169e6b90
Itajacy BSD <itajacy@bsd.com.br>
2023-12-12 10:34:40 -0300

    implementada a navegacao com o metodo getPage()

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/core/ui/todo_list_icons.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
 import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
@@ -6,9 +7,21 @@ import 'package:todo_list_provider/app/modules/home/widgets/home_filters.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_header.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_tasks.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_week_filter.dart';
+import 'package:todo_list_provider/app/modules/tasks/task_create_page.dart';
+import 'package:todo_list_provider/app/modules/tasks/tasks_module.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  void _goToCreateTask(BuildContext context) {
+    // Navigator.of(context).pushNamed('/task/create');
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => TasksModule().getPage('/task/create', context),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +43,7 @@ class HomePage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: context.primaryColor,
-        onPressed: () {},
+        onPressed: () => _goToCreateTask(context),
         child: Icon(Icons.add),
       ),
       backgroundColor: Color(0xFFFAFBFE),
