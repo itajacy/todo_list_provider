8f7045653dd7ae724f17e870d0060e14f4239f57
Itajacy BSD <itajacy@bsd.com.br>
2023-12-12 11:54:26 -0300

    finalizada aula de construcao de layou e animacao de transicao

--- lib/app/modules/tasks/task_create_page.dart
+++ lib/app/modules/tasks/task_create_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/widget/todo_list_field.dart';
 import 'package:todo_list_provider/app/modules/tasks/task_create_controller.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+import 'package:todo_list_provider/app/modules/tasks/widgets/calendar_button.dart';
 
 class TaskCreatePage extends StatelessWidget {
   TaskCreateController _controller;
@@ -11,7 +14,54 @@ class TaskCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: Icon(
+              Icons.close,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: context.primaryColor,
+        onPressed: () {},
+        label: Text(
+          'Salvar Task',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Form(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Criar Atividade',
+                  style: context.titleStyle.copyWith(fontSize: 20),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TodoListField(label: ''),
+              SizedBox(
+                height: 20,
+              ),
+              CalendarButton(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
