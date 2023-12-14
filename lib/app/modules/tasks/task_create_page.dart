bc27386d3e27a5c61e689688b6457bd120a56d7f
Itajacy BSD <itajacy@bsd.com.br>
2023-12-14 09:42:26 -0300

    calendar_button.dart alterado e funcionando

--- lib/app/modules/tasks/task_create_page.dart
+++ lib/app/modules/tasks/task_create_page.dart
@@ -3,9 +3,11 @@ import 'package:todo_list_provider/app/core/widget/todo_list_field.dart';
 import 'package:todo_list_provider/app/modules/tasks/task_create_controller.dart';
 import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
 import 'package:todo_list_provider/app/modules/tasks/widgets/calendar_button.dart';
+import 'package:validatorless/validatorless.dart';
 
 class TaskCreatePage extends StatelessWidget {
-  TaskCreateController _controller;
+  final TaskCreateController _controller;
+  final descriptionEC = TextEditingController();
 
   TaskCreatePage({Key? key, required TaskCreateController controller})
       : _controller = controller,
@@ -53,7 +55,11 @@ class TaskCreatePage extends StatelessWidget {
               SizedBox(
                 height: 30,
               ),
-              TodoListField(label: ''),
+              TodoListField(
+                label: '',
+                controller: descriptionEC,
+                validator: Validatorless.required('Descrição Obrigatória'),
+              ),
               SizedBox(
                 height: 20,
               ),
--- lib/app/modules/tasks/widgets/calendar_button.dart
+++ lib/app/modules/tasks/widgets/calendar_button.dart
@@ -6,26 +6,39 @@ class CalendarButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // width: 230,
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(
-            Icons.today,
-            color: Colors.grey,
-          ),
-          SizedBox(width: 10),
-          Text(
-            'SELECIONE UMA DATA',
-            style: context.titleStyle,
-          ),
-        ],
+    return InkWell(
+      onTap: () async {
+        var lastDate = DateTime.now();
+        lastDate = lastDate.add(Duration(days: 10 * 365));
+        final DateTime? selectedDate = await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(2000),
+          lastDate: lastDate,
+        );
+      },
+      borderRadius: BorderRadius.circular(30),
+      child: Container(
+        // width: 230,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.today,
+              color: Colors.grey,
+            ),
+            SizedBox(width: 10),
+            Text(
+              'SELECIONE UMA DATA',
+              style: context.titleStyle,
+            ),
+          ],
+        ),
       ),
     );
   }
