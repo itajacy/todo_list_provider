36bb264abb26f41cf1f5d2d96845501820c27602
itajacy <itajacy@gmail.com>
2023-12-21 16:09:28 -0300

    Buscas de tasks repository e services finalizada

--- lib/app/repositories/tasks/tasks_repository.dart
+++ lib/app/repositories/tasks/tasks_repository.dart
@@ -1,3 +1,7 @@
+import 'package:todo_list_provider/app/models/task_model.dart';
+
 abstract interface class TasksRepository {
   Future<void> save(DateTime date, String description);
+
+  Future<List<TaskModel>> findByPeriod(DateTime start, DateTime end);
 }
